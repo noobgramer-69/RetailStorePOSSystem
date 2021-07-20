fc1b46303bbf5375daeb2149c9f730e2bb5acac4
noobgramer-69 <76022316+noobgramer-69@users.noreply.github.com>
2021-07-20 15:52:26 +0800

    hellogit

---
+++ OODJApplication/src/oodjapplication/OODJApplication.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oodjapplication;
+
+/**
+ *
+ * @author tsj09
+ */
+public class OODJApplication {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello GitHub");
+    }
+    
+}
