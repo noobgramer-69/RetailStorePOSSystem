e6a9ab711300cbbdff31551f7558db380a86d647
noobgramer-69 <76022316+noobgramer-69@users.noreply.github.com>
2021-07-21 22:09:50 +0800

    added

--- Assignment/src/assignment/Assignment.java
+++ Assignment/src/assignment/Assignment.java
@@ -17,6 +17,7 @@ public class Assignment {
     public static void main(String[] args) {
         System.out.println("Hello jackson");
         System.out.println("hi");
+        System.out.println("Admin");
     }
     
 }
